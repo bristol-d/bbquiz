f168e21e4b74f0fb6a526616810ea41f0f731932
bristol-d <->
2020-11-02 10:19:43 +0000

    test script

---
+++ unittest.bat
@@ -0,0 +1,18 @@
+@rem Run a unit test for a particular sample file
+@setlocal
+
+cd samples
+@set key=%1
+@set z=C:\Progra~1\7-Zip\7z.exe
+del %key%.zip
+python ../src/main.py %key%.qq
+cd out
+rmdir /S /Q %key%
+mkdir %key%
+cd %key%
+%z% x ..\..\%key%.zip
+cd ..
+cd ..
+cd ..
+
+@endlocal
