0c2176e3c4bf1e1def79a0841a9f799bdfd7e352
bristol-d <->
2020-11-26 14:12:35 +0000

    first attempt for BB

--- src/questions.py
+++ src/questions.py
@@ -2,10 +2,12 @@ import mcq
 import shortanswer
 import maq
 import numeric
+import jumbled
 
 QUESTION_TYPES = {
     'mcq': mcq.Mcq,
     'shortanswer': shortanswer.ShortAnswer,
     'maq': maq.Maq,
-    'numeric': numeric.Numeric
+    'numeric': numeric.Numeric,
+    'jumbled': jumbled.Jumbled
 }
