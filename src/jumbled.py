a97aaecbddb704cd9c0447e8bcbada57bb3a192a
David <->
2022-03-15 11:36:12 +0000

    remove walrus operator for backward como

--- src/jumbled.py
+++ src/jumbled.py
@@ -3,7 +3,6 @@ import uuid
 from renderer import template_filename
 import question
 import re
-import html
 
 class JumbledOption:
     def __init__(self, text):
@@ -15,13 +14,12 @@ class Jumbled(question.Question):
     self.options holds a list of (text, hex) options.
     self.mapping holds a map tag => index
     """
-    def __init__(self, confopts = {'partial': 'false'}):
-        super().__init__(confopts)
+    def __init__(self):
+        super().__init__(confopts = {'partial': 'false'})
         self.options = []
         self.partial = 'false'
         self.counter = 0
         self.mapping = {}
-        self.subtype = "jumbled sentence"
         
     def parse2(self, parser, command, arg):
         if command == 'text':
@@ -49,71 +47,56 @@ class Jumbled(question.Question):
             else:
                 parser._raise(f"Illegal value for 'partial', use 'true' or 'false' (question starting at line {self.startline})")
 
-        return self
-
-    def tagger(self):
-        """
-        Return a closure that, on each call, gets the next tag in the sequence
-        a, b, ..., z, aa, ab, ... zz
-
-        Calling with parameter 'True' returns the current counter value.
-        """
-        c = 0
-        def closure(peek = None):
-            nonlocal c
-            if peek: return c
-            alphabet = "abcdefghijklmnopqrstuvwxyz"
-            l = len(alphabet)
-            r = c % l
-            q = int((c-r)/l)
-            if q == 0:
-                tag = alphabet[r:r+1]
-            else:
-                tag = alphabet[q-1:q] + alphabet[r:r+1]
-            c += 1
-            return tag
-        return closure
-
-    def replace(self, text):
-        """
-        Replace {i} placeholders with [a], [b], ...
-        Return the replaced text and the number of tags found.
-        """
+        # parse the text and replace {i} identifiers
+        # doing this here so we can throw a parser exception with a line number if needed
+        text = self.text
         expr = r'[{]([0-9]+)[}]'
-        counter = 0
-        mapping = {}
-        tags = self.tagger()
-        while (m := re.search(expr, text)):
-            ii = m.group(1)
-            i = int(ii)
-            if i <= 0 or i > len(self.options): 
-                raise Exception(
-                    f"In {self.subtype} text question starting at line {self.startline}: " +
+        while True:
+            m = re.search(expr, text)
+            if not m:
+                break
+            i = m.group(1)
+            try:
+                i = int(i)
+                if i <= 0 or i > len(self.options): raise ValueError()
+            except ValueError:
+                parser._raise(
+                    f"In jumbled text question starting at line {self.startline}: " +
                     "Invalid format specifier in question text: must be " +
-                    f"{i} where i is an integer between 1 and the number of options. " +
-                    f"I got: '{ii}' (parsed as integer: {i})" +
-                    f"Text is: \n{text}\n"
+                    "{i} where i is an integer between 1 and the number of options."
                 )
-            tag = tags()
+            tag = self.next_tag()
             self.mapping[tag] = i
             text = re.sub(expr, f"[{tag}]", text, 1)
-        return text, tags(True)
+        self.text2 = text
 
+        return self
+
+    def next_tag(self):
+        """
+        Get the next tag in the sequence a, b, ..., z, aa, ab, ... zz
+        """
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        l = len(alphabet)
+        c = self.counter
+        r = c % l
+        q = int((c-r)/l)
+        if q == 0:
+            tag = alphabet[r:r+1]
+        else:
+            tag = alphabet[q-1:q] + alphabet[r:r+1]
+        self.counter += 1
+        return tag
 
     def render(self, qn, idgen, renderer):
         template = Template(filename = template_filename("jumbled"))
-        text = renderer.render_text(self.text)
-        self.rendered, self.counter = self.replace(text)
+        self.rendered = renderer.render_text(self.text2)
         return template.render(
             question = self,
             id = idgen(),
             title = qn
         )
 
-    def display(self, fmt, subtype = "Jumbled sentence"):
+    def display(self, fmt):
         t = Template(filename = template_filename("html_jumbled"))
-        text = fmt(self.text)
-        text, c2 = self.replace(text)
-        assert c2 == self.counter, "Tag mismatch between HTML and BB versions."
-        self.htmltext = text      
-        return t.render(question = self, fmt = fmt, subtype = subtype)
+        return t.render(question = self, fmt = fmt)
